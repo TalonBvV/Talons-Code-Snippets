8e003af646be12408d0199944bb69ab91e134ad3
Talon Bernard van Vuuren <talbvvtrueuser9174@gmail.com>
2023-09-16 15:52:20 +0200

    ReactGrid-Snippet

---
+++ ReactGrid/Grid.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+function GenerateGridStyles(gap, columns, rows) {
+    let styles = { width: '', height: '', display: '', gap: '', gridTemplateColumns: '', gridTemplateRows: '' };
+    styles.width = '100%';
+    styles.height = '100%';
+    styles.display = 'grid';
+    styles.gap = gap;
+    styles.gridTemplateColumns = '';
+    styles.gridTemplateRows = '';
+    for (let i = 0; i < columns.length; i++) {
+        if (i === columns.length - 1) {
+            styles.gridTemplateColumns += columns[i].width;
+        }
+        else {
+            styles.gridTemplateColumns += columns[i].width + ' ';
+        }
+    }
+    for (let i = 0; i < rows.length; i++) {
+        if (i === rows.length - 1) {
+            styles.gridTemplateRows += rows[i].height;
+        }
+        else {
+            styles.gridTemplateRows += rows[i].height + ' ';
+        }
+    }
+    return styles;
+}
+function GenerateElementStyles(element) {
+    let styles = { gridColumn: '', gridRow: '' };
+    if (element.props.start_column) {
+        styles.gridColumn = element.props.start_column;
+    }
+    if (element.props.end_column) {
+        styles.gridColumn += ' / ' + element.props.end_column;
+    }
+    if (element.props.start_row) {
+        styles.gridRow = element.props.start_row;
+    }
+    if (element.props.end_row) {
+        styles.gridRow += ' / ' + element.props.end_row;
+    }
+    return styles;
+}
+
+function Grid(props) {
+    const styles = GenerateGridStyles(props.gap, props.columns, props.rows);
+
+    return (
+        <div style={styles}>
+            {
+                React.Children.map(props.children, child => (
+                    <div style={GenerateElementStyles(child)}>
+                        {child}
+                    </div>
+                ))
+            }
+        </div>
+    );
+}
+
+export default Grid;
---
+++ ReactGrid/exampe.js
@@ -0,0 +1,17 @@
+import Grid from './Components/Grid';
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <Grid columns={[{ width: '200px' }, { width: '200px' }]} rows={[{ height: '100px' }]}>
+                    <div start_column='1' end_column='3' start_row='1'>
+                        Hello There
+                    </div>
+                </Grid>
+            </div>
+        </div>
+    );
+}
+
+export default App;
